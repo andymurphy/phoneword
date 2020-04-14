08a9b21dd221f2aabe97d3b61b511e61a4404e4f
Andy <amulder964@aol.com>
2020-04-14 14:03:57 +0100

    Used the shared resources in the XAML file to colour the buttons on the UI

--- Phoneword/Phoneword/SharedResources.cs
+++ Phoneword/Phoneword/SharedResources.cs
@@ -7,6 +7,11 @@ namespace Phoneword
 {
     public static class SharedResources
     {
-        public static Color ButtonBkColour = Color.FromArgb(0xff, 0xa5, 0); // Hexadecimal notation -> (255, 165, 0) in decimal
+        // Do these two work the same. Perhaps the 2nd one cannot be changed, only accessed - no setter.
+        public static Color ButtonBkColour = Color.FromArgb(0xff, 0xa5, 0); // Hexadecimal notation -> (255, 165, 0) in decimal. Red/green
+        public static Color ButtonTextColour
+        {
+            get { return Color.White;  }
+        }
     }
 }
